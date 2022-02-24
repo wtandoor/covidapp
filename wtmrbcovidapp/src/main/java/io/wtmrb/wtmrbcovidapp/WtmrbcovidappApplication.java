67f384f284fd179e24bd7cc29e9faf09d533e440
wtandoor <80604043+wtandoor@users.noreply.github.com>
2022-02-24 14:03:02 +0300

    Add files via upload

---
+++ wtmrbcovidapp/src/main/java/io/wtmrb/wtmrbcovidapp/WtmrbcovidappApplication.java
@@ -0,0 +1,12 @@
+package io.wtmrb.wtmrbcovidapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtmrbcovidappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WtmrbcovidappApplication.class, args);
+	}
+}
